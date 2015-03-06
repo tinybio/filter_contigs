fa581e801f1dbe51a867c7278b809e85bd0a3fbe
Justin Payne <justin.payne@fda.hhs.gov>
2015-03-06 09:58:51 -0500

    initial commit

---
+++ filter_contigs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+from Bio import SeqIO
+min_length, fasta_file_path = sys.argv[1:]
+with open(fasta_file_path.replace('fasta', 'filter{}.fasta'.format(min_length)), 'w') as filtered_fasta:
+	with open(fasta_file_path, 'rU') as input_fasta:
+		def filtered_contigs_generator(min):
+			for contig in SeqIO.parse(input_fasta, 'fasta'):
+				if len(contig) >= min:
+					yield contig
+		SeqIO.write(filtered_contigs_generator(int(min_length)), filtered_fasta, 'fasta')
