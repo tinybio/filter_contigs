44b289ab931f151ce877e4741bcd2a3f7e4a136f
Justin Payne <crashfrog@gmail.com>
2016-04-01 14:27:46 -0400

    Update filter_contigs.py
    slightly safer behavior if people use '.fa' as a fasta file extension.

--- filter_contigs.py
+++ filter_contigs.py
@@ -2,7 +2,7 @@
 import sys
 from Bio import SeqIO
 min_length, fasta_file_path = sys.argv[1:]
-with open(fasta_file_path.replace('fasta', 'filter{}.fasta'.format(min_length)), 'w') as filtered_fasta:
+with open(fasta_file_path.replace('fa', 'filter{}.fa'.format(min_length)), 'w') as filtered_fasta:
 	with open(fasta_file_path, 'rU') as input_fasta:
 		def filtered_contigs_generator(min):
 			for contig in SeqIO.parse(input_fasta, 'fasta'):
